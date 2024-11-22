a805ddc7320437b7dde04925879370fdbfd7d814
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2024-11-23 02:18:10 +0530

    Add home-page API with controller, service, and routes; update frontend page structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const getStrapiData = async (url: string) => {
+  const baseUrl = `http://localhost:1337`
+
+  try {
+    const response = await fetch(`${baseUrl}${url}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  const { title, description } = strapiData.data;
   return (
     <main className="container mx-auto py-6">
-      <Button>Click me</Button>
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
     </main>
   );
 }
